f1d5749d1a950503e4e998c1f8e45e73e18ce009
Devin Doherty <devincdoherty@gmail.com>
2020-10-12 16:46:24 -0400

    Initial commit
    -add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+
+def intro():
+    print ("Dust. A planet of sand and ruin. A desert planet.\n" 
+    "It is the most important planet in the Third Imperium,\n" 
+    "For the sands of Dust are like nothing else in the universe.\n"
+    "You are the new Duke of Dust, entrusted by the Emperor to govern the as yet\n" 
+    "ungovernable world and rule in his name.\n"
+    "What ambitions might you have?")
+
+intro()
+
+menu = {}
+menu['1'] = "Household"
+menu['2'] = "Council"
+menu['3'] = "Diplomacy"
+menu['4'] = "Warfare"
+menu['5'] = "Factions"
+menu['6'] = "Intrigue"
+menu['7'] = ""
+
+#print(NewGame)
