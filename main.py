7e927321b1e4f84838a6380cb5404c284aaef4b9
Devin Doherty <devincdoherty@gmail.com>
2020-10-19 04:38:00 -0400

    Update main.py
    - Merged cursedtest.py menu with main.py
    - Work on adding intro before menu next

--- main.py
+++ main.py
@@ -1,22 +1,67 @@
 import time
+import curses
+import time
+import sys
+
+def menu(root, current_row):
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    height, width = root.getmaxyx()
+    
+    menu = [ "Household", "Council", "Diplomacy", "Warfare", "Intrigue", "Economy"]
+    
+    for idx, element in enumerate(menu):
+        y = height // 2 + idx
+        x = width // 2 - len(element) // 2 
+
+        if idx == current_row:
+            root.attron(curses.color_pair(1))
+            root.addstr(y, x, element)
+            root.attroff(curses.color_pair(1))
+
+        else:
+            root.addstr(y, x, element)
+
+    root.refresh()
+#   time.sleep(5)
+
+def main(root):
+    curses.curs_set(0)
+
+    current_row = 0
+
+    menu(root, current_row)
+
+    while True:
+        key = root.getch()
+
+        if key == curses.KEY_UP and current_row > 0:
+           current_row -= 1 
+        
+        elif key == curses.KEY_DOWN and current_row < 5:
+            current_row += 1
+
+        elif key == ord("e"):
+            root.refresh()
+            root.addstr(0, 0, "The Letter E")
+        
+        elif key == ord("q"):
+           break
+
+        menu(root, current_row)
+
+    root.refresh()
+
+
+curses.wrapper(main)
 
 def intro():
     print ("Dust. A planet of sand and ruin. A desert planet.\n" 
     "It is the most important planet in the Third Imperium,\n" 
     "For the sands of Dust are like nothing else in the universe.\n"
-    "You are the new Duke of Dust, entrusted by the Emperor to govern the as yet\n" 
+    "You are the new Duke of Dust," 
+    "entrusted by the Emperor to govern the as yet\n" 
     "ungovernable world and rule in his name.\n"
     "What ambitions might you have?")
 
 intro()
-
-menu = {}
-menu['1'] = "Household"
-menu['2'] = "Council"
-menu['3'] = "Diplomacy"
-menu['4'] = "Warfare"
-menu['5'] = "Factions"
-menu['6'] = "Intrigue"
-menu['7'] = ""
-
-#print(NewGame)
+       
