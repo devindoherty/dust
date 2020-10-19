596c5603bf34946d1cc34dfdadc7edd0edcc105c
Devin Doherty <devincdoherty@gmail.com>
2020-10-19 07:07:18 -0400

    Add pytests.py
    - Added a file called pytests for python learning purposes
    - Playing around with inputs

---
+++ pytests.py
@@ -0,0 +1,15 @@
+# Learning python, mostly.
+
+some_var = int(input("Give me a number, any number: "))
+
+if some_var > 10:
+    print("The number is bigger than tenerooonie.")
+elif some_var < 10:
+    print("The numbah is smaller than 10.")
+elif some_var == 10:
+        ("YO. The numbah is TEN!")
+
+
+some_name = str(input("Hey, what's your name, kid: "))
+
+print("Pleasure to meet ya, " + some_name)
